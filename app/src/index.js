8f76716d7d7c3bdf306ab398fbe1680ea8e06065
Rochelle Lewis <rlewis37@gmail.com>
2019-06-09 22:22:15 -0600

    Add Home page, 404 components

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
 
-const App = () => ( <h1 className="text-info">hello world</h1> );
-ReactDOM.render(<App/>, document.querySelector('#root'));
+import {FourOhFour} from "./pages/FourOhFour";
+import {Home} from "./pages/Home";
+
+const Routing = () => (
+	<>
+
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route component={FourOhFour}/>
+			</Switch>
+		</BrowserRouter>
+
+	</>
+);
+
+ReactDOM.render(<Routing/>, document.querySelector('#root'));
---
+++ app/src/pages/FourOhFour.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const FourOhFour = () => {
+	return (
+		<>
+			<h1>404 Error: Y U NO FIND?</h1>
+		</>
+	)
+};
