39fbcee9ac86b6cefe57dbc1a3a655d73c3d7880
Rochelle Lewis <rlewis37@gmail.com>
2019-08-16 02:18:04 -0600

    update DDL with new attributes per DDC curriculm changes

--- sql/creepyoctomeow.sql
+++ sql/creepyoctomeow.sql
@@ -8,11 +8,9 @@ DROP TABLE IF EXISTS profile;
 CREATE TABLE profile (
 	profileId BINARY(16) NOT NULL,
 	profileActivationToken CHAR(32),
-	profileEmail VARCHAR(64) NOT NULL,
-	profileHash CHAR(128) NOT NULL,
-	profileSalt CHAR(64) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL,
+	profileHash CHAR(97) NOT NULL,
 	profileUsername VARCHAR(64) NOT NULL,
-	UNIQUE (profileActivationToken),
 	UNIQUE (profileEmail),
 	UNIQUE (profileUsername),
 	PRIMARY KEY (profileId)
