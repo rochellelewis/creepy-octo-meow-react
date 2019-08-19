6a17d64731411a0092e9aadb8c0000a1b24b2087
Rochelle Lewis <rlewis37@gmail.com>
2019-08-19 01:27:19 -0600

    add base files for new static ui

---
+++ static-ui/static-ui-2019/home.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+	<?php
+	$CURRENT_DIR = __DIR__;
+	$PAGE_TITLE = "Meow App 6 | An React Demo Project";
+	require_once("./partials/head-utils.php");
+	?>
+	<body>
+		Home page template
+	</body>
+</html>
