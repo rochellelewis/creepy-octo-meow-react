a4e51d32bb1a90f7b882d445ef6cb3e2bee2b1eb
Rochelle Lewis <rlewis37@gmail.com>
2019-08-20 21:04:09 -0600

    delete superfluous require_once

--- php/public_html/apis/profile/index.php
+++ php/public_html/apis/profile/index.php
@@ -2,7 +2,6 @@
 require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
 require_once (dirname(__DIR__, 3) . "/Classes/autoload.php");
 require_once (dirname(__DIR__, 3) . "/lib/xsrf.php");
-require_once (dirname(__DIR__, 3) . "/lib/uuid.php");
 require_once (dirname(__DIR__, 3) . "/lib/jwt.php");
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 
