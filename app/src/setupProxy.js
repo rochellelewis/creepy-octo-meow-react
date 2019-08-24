7c11c8ef9e53a80097b5604425b9ef0c64592740
Rochelle Lewis <rlewis37@gmail.com>
2019-08-24 04:44:16 -0600

    fix target path

--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
 	app.use(proxy('/apis', {
 		logLevel: 'debug',
-		target: "https://bootcamp-coders.cnm.edu/~rlewis37/creepy-octo-meow-react/public_html/",
+		target: "https://bootcamp-coders.cnm.edu/~rlewis37/creepy-octo-meow-react/php/public_html/",
 		changeOrigin: true,
 		secure: true,
 
