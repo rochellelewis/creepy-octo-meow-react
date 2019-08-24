9013ea05ed2b8fd42300319003bf6a5425a64033
Rochelle Lewis <rlewis37@gmail.com>
2019-08-24 17:22:14 -0600

    refactor footer to use React Bootstrap layout components

--- app/src/shared/components/Footer.js
+++ app/src/shared/components/Footer.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
@@ -11,13 +12,13 @@ export const Footer = () => (
 		<footer className="page-footer text-muted py-2 py-md-4">
 			<Container fluid="true">
 				<Row>
-					<div className="col-sm-6 text-center text-sm-left small">
+					<Col sm="6" className="text-center text-sm-left small">
 						Octo Meow 7.0 => A DDC React Demo.
-					</div>
-					<div className="col-sm-6 text-center text-sm-right small">
+					</Col>
+					<Col sm="6" className="text-center text-sm-right small">
 						<FontAwesomeIcon icon={['fab','github']}/>&nbsp;
 						<a href="https://github.com/rlewis2892/creepy-octo-meow-react" className="text-muted" target="_blank" rel="noopener noreferrer">View on GitHub</a> | <Link className="text-muted" to="/about">About Us</Link>
-					</div>
+					</Col>
 				</Row>
 			</Container>
 		</footer>
