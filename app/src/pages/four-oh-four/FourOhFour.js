d3667f6b05bacca53b05cf59eee3ace959420cab
Rochelle Lewis <rlewis37@gmail.com>
2019-08-23 21:20:42 -0600

    update 404 and About templates, css

--- app/src/pages/four-oh-four/FourOhFour.js
+++ app/src/pages/four-oh-four/FourOhFour.js
@@ -1,9 +1,21 @@
 import React from "react"
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 export const FourOhFour = () => {
 	return (
 		<>
-			<h1>404 Error: Y U NO FIND?</h1>
+			<main className="py-5">
+				<Container fluid="true">
+					<Row>
+						<Col>
+							<h1>404 Error: Y U NO FIND?</h1>
+						</Col>
+					</Row>
+				</Container>
+			</main>
 		</>
 	)
 };
