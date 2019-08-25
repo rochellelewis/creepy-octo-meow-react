7c08dd1727cf3f541731a26f88230ca421da12b9
Rochelle Lewis <rlewis37@gmail.com>
2019-08-25 04:56:32 -0600

    create profile reducer

--- app/src/shared/reducers/index.js
+++ app/src/shared/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux"
 import postReducer from "./post-reducer";
+import profileReducer from "./profile-reducer";
 
 export default combineReducers({
-	posts: postReducer
+	posts: postReducer,
+	profile: profileReducer,
 })
