3d4268cad1474ff65b08e96bef174644c3959b91
Rochelle Lewis <rlewis37@gmail.com>
2019-08-27 10:51:06 -0600

    update reducers

--- app/src/shared/reducers/profile-reducer.js
+++ app/src/shared/reducers/profile-reducer.js
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
 	switch(action.type) {
+		case "GET_ALL_PROFILES":
+			return action.payload;
 		case "GET_PROFILE_BY_PROFILE_ID":
 			return action.payload;
 		case "GET_PROFILE_BY_PROFILE_EMAIL":
