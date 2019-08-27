0d6efdea140aa42f6176c26e3e001299c2ef5964
Rochelle Lewis <rlewis37@gmail.com>
2019-08-27 10:47:59 -0600

    update actions

---
+++ app/src/shared/actions/get-post.js
@@ -0,0 +1,22 @@
+import {httpConfig} from "../misc/http-config";
+import _ from "lodash";
+import {getProfileByProfileId} from "./get-profile";
+
+export const getAllPosts = () => async dispatch => {
+	const {data} = await httpConfig('/apis/post/');
+	dispatch({type: "GET_ALL_POSTS", payload: data })
+};
+
+export const getPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(getAllPosts());
+
+	const profileIds = _.uniq(_.map(getState().posts, "profileId"));
+	profileIds.forEach(id => dispatch(getProfileByProfileId(id)));
+
+	//commented out lines below are equivalent to the _ chain method
+	// _.chain(getState().posts)
+	// 	.map("postProfileId")
+	// 	.uniq()
+	// 	.forEach(id => dispatch(getProfileByProfileId(id)))
+	// 	.value()
+};
--- app/src/shared/actions/get-profile.js
+++ app/src/shared/actions/get-profile.js
@@ -1,5 +1,10 @@
 import {httpConfig} from "../misc/http-config";
 
+export const getAllProfiles = () => async dispatch => {
+	const {data} = await httpConfig('/apis/profile/');
+	dispatch({type: "GET_ALL_PROFILES", payload: data })
+};
+
 export const getProfileByProfileId = (id) => async dispatch => {
 	const {data} = await httpConfig(`/apis/profile/${id}`);
 	dispatch({type: "GET_PROFILE_BY_PROFILE_ID", payload: data })
