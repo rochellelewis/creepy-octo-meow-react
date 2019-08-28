bc5d4266857a44148c8891cefb4f8b7f9f9de0c1
Rochelle Lewis <rlewis37@gmail.com>
2019-08-28 17:07:03 -0600

    fix footer structure

--- app/src/shared/components/Footer.js
+++ app/src/shared/components/Footer.js
@@ -6,20 +6,22 @@ import Row from "react-bootstrap/Row";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-export const Footer = () => (
-	<>
-		<footer className="page-footer text-muted py-2 py-md-4">
-			<Container fluid="true">
-				<Row>
-					<div className="col-sm-6 text-center text-sm-left small">
-						Octo Meow 7.0 => A DDC React Demo.
-					</div>
-					<div className="col-sm-6 text-center text-sm-right small">
-						<FontAwesomeIcon icon={['fab','github']}/>&nbsp;
-						<a href="https://github.com/rlewis2892/creepy-octo-meow-react" className="text-muted" target="_blank" rel="noopener noreferrer">View on GitHub</a> | <Link className="text-muted" to="/about">About Us</Link>
-					</div>
-				</Row>
-			</Container>
-		</footer>
-	</>
-);
+export const Footer = () => {
+	return (
+		<>
+			<footer className="page-footer text-muted py-2 py-md-4">
+				<Container fluid="true">
+					<Row>
+						<Col sm="6" className="text-center text-sm-left small">
+							Octo Meow 7.0 => A DDC React Demo.
+						</Col>
+						<Col sm="6" className="text-center text-sm-right small">
+							<FontAwesomeIcon icon={['fab','github']}/>&nbsp;
+							<a href="https://github.com/rlewis2892/creepy-octo-meow-react" className="text-muted" target="_blank" rel="noopener noreferrer">View on GitHub</a> | <Link className="text-muted" to="/about">About Us</Link>
+						</Col>
+					</Row>
+				</Container>
+			</footer>
+		</>
+	)
+};
