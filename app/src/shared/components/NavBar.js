3a156788d68aab8be8fe9eae24edc554485bac7a
Rochelle Lewis <rlewis37@gmail.com>
2019-08-28 02:04:28 -0600

    unfuckify navbar component structure :stuck_out_tongue:, make placeholder for signout

--- app/src/shared/components/NavBar.js
+++ app/src/shared/components/NavBar.js
@@ -5,39 +5,48 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
-
+import Form from "react-bootstrap/Form";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-export const NavBar = () => (
-	<>
-		<header>
-			<Navbar bg="light" expand="md" variant="light" fixed="top">
-				<Link to="/">
-					<Navbar.Brand>=^ Octo Meow 7.0 ^=</Navbar.Brand>
-				</Link>
-				<Navbar.Text className="small font-italic d-none d-md-inline-block">A DDC React Demo.</Navbar.Text>
-				<Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
-				<Navbar.Collapse>
-					<Nav className="ml-auto">
+export const NavBar = () => {
 
-						<NavDropdown className="nav-link" title={"Welcome, --username--- !"}>
-							<NavDropdown.Item href="/profile">
-								<FontAwesomeIcon icon="user"/>&nbsp; Profile
-							</NavDropdown.Item>
-							<div className="dropdown-divider"></div>
-							<NavDropdown.Item href="/signout">
-								<FontAwesomeIcon icon="sign-out-alt"/>&nbsp; Sign Out
-							</NavDropdown.Item>
-						</NavDropdown>
+	const signOut = () => {
+		console.log("create signout :P");
+	};
 
-						<Link className="nav-link" to="/posts">
-							<Button variant="outline-dark" className="btn-block">
-								<FontAwesomeIcon icon="cat"/>&nbsp;Posts
-							</Button>
-						</Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
-		</header>
-	</>
-);
+	return (
+		<>
+			<header>
+				<Navbar bg="light" expand="md" variant="light" fixed="top">
+					<Link to="/">
+						<Navbar.Brand>=^ Octo Meow 7.0 ^=</Navbar.Brand>
+					</Link>
+					<Navbar.Text className="small font-italic d-none d-md-inline-block">A DDC React Demo.</Navbar.Text>
+					<Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+					<Navbar.Collapse>
+						<Nav className="ml-auto">
+
+							<NavDropdown className="nav-link" title={"Welcome, --username--- !"}>
+								<NavDropdown.Item href="/profile">
+									<FontAwesomeIcon icon="user" />&nbsp; Profile
+								</NavDropdown.Item>
+								<div className="dropdown-divider"></div>
+								<Form className="dropdown-item">
+									<Button variant="outline-dark" type="submit" onClick={signOut()}>
+										Sign Out&nbsp;<FontAwesomeIcon icon="sign-out-alt" />
+									</Button>
+								</Form>
+							</NavDropdown>
+
+							<Link className="nav-link" to="/posts">
+								<Button variant="outline-dark" className="btn-block">
+									<FontAwesomeIcon icon="cat" />&nbsp;Posts
+								</Button>
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			</header>
+		</>
+	)
+};
