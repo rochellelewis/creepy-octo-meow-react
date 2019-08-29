d9bbd2a90206c0c96c015b75f6a24ad58d90b197
Rochelle Lewis <rlewis37@gmail.com>
2019-08-29 14:48:52 -0600

    add conditional render of post form if jwt !== null

--- app/src/pages/posts/Posts.js
+++ app/src/pages/posts/Posts.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from "react";
+import {Link} from "react-router-dom";
 import {useSelector, useDispatch} from "react-redux";
 
 import {PostForm} from "./PostForm";
@@ -10,6 +11,7 @@ import {UseWindowWidth} from "../../shared/components/UseWindowWidth";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Accordion from "react-bootstrap/Accordion";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -53,20 +55,31 @@ export const Posts = () => {
 						{/*BEGIN FORM PANEL*/}
 						<Col md={4} className="posts-form-panel position-fixed">
 
-							{/* This ternary will render the PostForm in either
+							{/* This nested ternary will render the PostForm only if jwt !== null,
+							otherwise show signin/signup links. Then render the post form in either
 							one of two different ways depending on the screen width.
 							This allows the rendering of this element to be responsive. */}
-							{width < 768 ? (
-								<Accordion defaultActiveKey="1" className="d-md-none">
-									<Accordion.Toggle as={Button} variant="primary" eventKey="0" className="btn-block">
-										<FontAwesomeIcon icon="pencil-alt"/>&nbsp;Write A Post
-									</Accordion.Toggle>
-									<Accordion.Collapse eventKey="0">
-										<PostForm/>
-									</Accordion.Collapse>
-								</Accordion>
+							{window.localStorage.getItem("jwt-token") !== null ? (
+								width < 768 ? (
+									<Accordion defaultActiveKey="1" className="d-md-none">
+										<Accordion.Toggle as={Button} variant="primary" eventKey="0" className="btn-block">
+											<FontAwesomeIcon icon="pencil-alt"/>&nbsp;Write A Post
+										</Accordion.Toggle>
+										<Accordion.Collapse eventKey="0">
+											<PostForm/>
+										</Accordion.Collapse>
+									</Accordion>
+									) : (
+									<PostForm/>
+								)
 							) : (
-								<PostForm/>
+								<Card bg="light" className="mb-3 text-center">
+									<Card.Body>
+										<h4 className="mb-3">Please log in to post a meow.</h4>
+										<Link to="/" className="btn btn-outline-dark mr-3">Sign In</Link>
+										<Link to="/signup" className="btn btn-dark">Sign Up</Link>
+									</Card.Body>
+								</Card>
 							)}
 
 						</Col>
