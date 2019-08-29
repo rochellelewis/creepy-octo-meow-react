371a301463f25afc749ef5af52a677833e0421d9
Rochelle Lewis <rlewis37@gmail.com>
2019-08-28 21:05:27 -0600

    finalize signup form - working!

--- app/src/pages/signup/Signup.js
+++ app/src/pages/signup/Signup.js
@@ -1,15 +1,11 @@
 import React from "react"
 
+import {SignUpForm} from "./SignUpForm";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/es/FormControl";
-import Button from "react-bootstrap/Button";
-
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Signup = () => {
 	return (
@@ -20,59 +16,7 @@ export const Signup = () => {
 						<Col sm={6} lg={{span: 4, offset: 1}}>
 							<Card bg="transparent" className="border-0 rounded-0">
 								<Card.Body>
-									<Form>
-
-										<Form.Group>
-											<InputGroup>
-												<InputGroup.Prepend>
-													<InputGroup.Text>
-														<FontAwesomeIcon icon="user"/>
-													</InputGroup.Text>
-												</InputGroup.Prepend>
-												<FormControl type="text" placeholder="Pick a User Name"/>
-											</InputGroup>
-										</Form.Group>
-
-										<Form.Group>
-											<InputGroup>
-												<InputGroup.Prepend>
-													<InputGroup.Text>
-														<FontAwesomeIcon icon="envelope"/>
-													</InputGroup.Text>
-												</InputGroup.Prepend>
-												<FormControl type="email" placeholder="Your Email"/>
-											</InputGroup>
-										</Form.Group>
-
-										<Form.Group>
-											<InputGroup>
-												<InputGroup.Prepend>
-													<InputGroup.Text>
-														<FontAwesomeIcon icon="key"/>
-													</InputGroup.Text>
-												</InputGroup.Prepend>
-												<FormControl type="password" placeholder="Password"/>
-											</InputGroup>
-										</Form.Group>
-
-										<Form.Group>
-											<InputGroup>
-												<InputGroup.Prepend>
-													<InputGroup.Text>
-														<FontAwesomeIcon icon="ellipsis-h"/>
-													</InputGroup.Text>
-												</InputGroup.Prepend>
-												<FormControl type="password" placeholder="Confirm Password"/>
-											</InputGroup>
-										</Form.Group>
-
-										<Form.Group className="text-md-right">
-											<Button variant="primary" type="submit">
-												<FontAwesomeIcon icon="paw"/>&nbsp;Join Us!
-											</Button>
-										</Form.Group>
-
-									</Form>
+									<SignUpForm/>
 								</Card.Body>
 							</Card>
 						</Col>
