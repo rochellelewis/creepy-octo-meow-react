85e0a6a2c017dc97e9155000a9cc3de5db186b4a
Rochelle Lewis <rlewis37@gmail.com>
2019-08-30 20:28:03 -0600

    correct errors and update like reducer :pencil2:

--- app/src/shared/actions/get-like.js
+++ app/src/shared/actions/get-like.js
@@ -1,16 +1,21 @@
 import {httpConfig} from "../misc/http-config";
 
+export const getAllLikes = () => async dispatch => {
+	const {data} = await httpConfig('/apis/like/');
+	dispatch({type: "GET_ALL_LIKES", payload: data })
+};
+
 export const getLikesByPostId = () => async dispatch => {
-	const {data} = await httpConfig('/apis/post/');
+	const {data} = await httpConfig('/apis/like/');
 	dispatch({type: "GET_LIKES_BY_POST_ID", payload: data })
 };
 
 export const getLikesByProfileId = () => async dispatch => {
-	const {data} = await httpConfig('/apis/post/');
+	const {data} = await httpConfig('/apis/like/');
 	dispatch({type: "GET_LIKES_BY_PROFILE_ID", payload: data })
 };
 
 export const getLikeByPostIdAndProfileId = (postId, profileId) => async dispatch => {
-	const {data} = await httpConfig(`/apis/post/?likePostId=${postId}&likeProfileId=${profileId}`);
+	const {data} = await httpConfig(`/apis/like/?likePostId=${postId}&likeProfileId=${profileId}`);
 	dispatch({type: "GET_LIKES_BY_POST_ID_AND_PROFILE_ID", payload: data })
 };
--- app/src/shared/reducers/like-reducer.js
+++ app/src/shared/reducers/like-reducer.js
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
 	switch(action.type) {
+		case "GET_ALL_LIKES":
+			return action.payload;
 		case "GET_LIKES_BY_POST_ID":
 			return action.payload;
 		case "GET_LIKES_BY_PROFILE_ID":
