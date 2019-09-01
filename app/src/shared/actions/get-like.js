9483df036f734d37686898e6a87353bb62da1a8f
Rochelle Lewis <rlewis37@gmail.com>
2019-09-01 06:33:36 -0600

    cleanup like actions/reducer

--- app/src/shared/actions/get-like.js
+++ app/src/shared/actions/get-like.js
@@ -4,18 +4,3 @@ export const getAllLikes = () => async dispatch => {
 	const {data} = await httpConfig('/apis/like/');
 	dispatch({type: "GET_ALL_LIKES", payload: data })
 };
-
-export const getLikesByPostId = (id) => async dispatch => {
-	const {data} = await httpConfig(`/apis/like/?likePostId=${id}`);
-	dispatch({type: "GET_LIKES_BY_POST_ID", payload: data })
-};
-
-export const getLikesByProfileId = (id) => async dispatch => {
-	const {data} = await httpConfig(`/apis/like/?likeProfileId=${id}`);
-	dispatch({type: "GET_LIKES_BY_PROFILE_ID", payload: data })
-};
-
-export const getLikeByPostIdAndProfileId = (postId, profileId) => async dispatch => {
-	const {data} = await httpConfig(`/apis/like/?likePostId=${postId}&likeProfileId=${profileId}`);
-	dispatch({type: "GET_LIKE_BY_POST_ID_AND_PROFILE_ID", payload: data })
-};
--- app/src/shared/reducers/like-reducer.js
+++ app/src/shared/reducers/like-reducer.js
@@ -2,12 +2,6 @@ export default (state = [], action) => {
 	switch(action.type) {
 		case "GET_ALL_LIKES":
 			return action.payload;
-		case "GET_LIKES_BY_POST_ID":
-			return {postLikes: action.payload};
-		case "GET_LIKES_BY_PROFILE_ID":
-			return {profileLikes: action.payload};
-		case "GET_LIKE_BY_POST_ID_AND_PROFILE_ID":
-			return [...state, {like: action.payload}];
 		default:
 			return state;
 	}
