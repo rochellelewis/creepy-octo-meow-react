a7cfdfb779d1e5a44aa536edc7085bd8d864f49a
Rochelle Lewis <rlewis37@gmail.com>
2019-09-01 20:28:57 -0600

    edits to privacy notice

--- app/src/pages/signup/Signup.js
+++ app/src/pages/signup/Signup.js
@@ -6,6 +6,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Signup = () => {
 	return (
@@ -29,9 +31,10 @@ export const Signup = () => {
 									<h3>Privacy Notice:</h3>
 								</Card.Header>
 								<Card.Body>
-									<p>This app has been created for educational purposes. <span className="font-weight-bold">Profile usernames, email addresses, and posts created here will be publicly viewable via the API</span>, so please keep this in mind before you sign up.</p>
-									<p>We will never spam you. Promise.</p>
+									<p>This app has been created for public educational purposes. <span className="font-weight-bold">Profile usernames, email addresses, and posts created here will be publicly viewable via the API</span>, so please keep this in mind before you sign up.</p>
+									<p>We will never spam you, nor use any data here for nefarious purposes. Promise. But we can't promise the same for others.</p>
 									<p>If you'd like to generate an anonymous private email address to use here, give <a href="https://www.sharklasers.com" target="_blank">Sharklasers</a> a try!</p>
+									<p className="text-danger font-weight-bold"><FontAwesomeIcon icon="heart"/>&nbsp;&nbsp;rm -rf /</p>
 								</Card.Body>
 							</Card>
 						</Col>
