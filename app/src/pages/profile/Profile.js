122c66a95dfb7cae18ad182641c084c49f0c6aef
Rochelle Lewis <rlewis37@gmail.com>
2019-09-01 19:46:31 -0600

    prevent display of private profile data if not logged into the same account. :closed_lock_with_key:

--- app/src/pages/profile/Profile.js
+++ app/src/pages/profile/Profile.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import {useSelector, useDispatch} from "react-redux";
+import {UseJwtProfileId} from "../../shared/misc/JwtHelpers";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -13,8 +14,12 @@ import {getProfileByProfileId} from "../../shared/actions/get-profile";
 
 export const Profile = ({match}) => {
 
+	// grab the profile id from the currently logged in account, or null if not found
+	const currentProfileId = UseJwtProfileId();
+
 	// Return the profile by profileId from the redux store
 	const profile = useSelector(state => (state.profile ? state.profile[0] : []));
+	console.log(profile);
 
 	const dispatch = useDispatch();
 
@@ -36,9 +41,17 @@ export const Profile = ({match}) => {
 									<h2 className="my-0">Hello, {profile && profile.profileUsername}!</h2>
 								</Card.Header>
 								<Card.Body>
-									<div>Your Email: {profile && profile.profileEmail}</div>
-									<div>Your Username: {profile && profile.profileUsername}</div>
-									<div>Your Activation Token: {(profile) && profile.profileActivationToken}</div>
+									<div>Username: {profile && profile.profileUsername}</div>
+
+									{/* only show the private profile data if logged into the same account! */}
+									{(profile && profile.profileId === currentProfileId) && (
+										<>
+											<div>Your Profile Id: {profile && profile.profileId}</div>
+											<div>Your Email Address: {profile && profile.profileEmail}</div>
+											<div>Your Activation Token: {(profile) && profile.profileActivationToken}</div>
+										</>
+									)}
+
 								</Card.Body>
 							</Card>
 						</Col>
