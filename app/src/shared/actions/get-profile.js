a2481e74461751dd99a7f7d318c38ebbee80b334
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-03-12 16:06:37 -0600

    remove unused api call from get-profile.js

--- app/src/shared/actions/get-profile.js
+++ app/src/shared/actions/get-profile.js
@@ -9,8 +9,3 @@ export const getProfileByProfileId = (id) => async dispatch => {
 	const {data} = await httpConfig(`/apis/profile/${id}`);
 	dispatch({type: "GET_PROFILE_BY_PROFILE_ID", payload: data })
 };
-
-export const getProfileByProfileEmail = (email) => async dispatch => {
-	const {data} = await httpConfig(`/apis/profile/?profileEmail=${email}`);
-	dispatch({type: "GET_PROFILE_BY_PROFILE_EMAIL", payload: data })
-};
